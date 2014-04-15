574a6e9383e6256e9531203593d339e345a8ab0e
cssgf3 <claytonsussman@yahoo.com>
2014-04-15 15:39:29 -0500

    Pages To End The Week and Save User Results
    the reviewLeagues.php page will go through each league, identify each
    roster, add the points up for each roster and figure out their ending
    place within the league. This will be stored along with their
    wins/losses updated in the previousGamePlace in the Users table.

---
+++ PHP Interraction/UpdatePlayerSlotIDPerWeek/reviewLeagues.php
@@ -0,0 +1,144 @@
+<?php
+include "dbconnect.php";
+
+$userToPoints = array();
+$tieArray = array();
+$totalPoints = 0;
+$k = 0;
+
+$leagueResult = pg_prepare($connection, "getLeagues", 'SELECT "id", "numPlayers" FROM leagues;');
+$leagueResult = pg_execute($connection, "getLeagues", array());
+//print_r(var_dump($leagueResult));
+//$leagueRow = pg_fetch_assoc($leagueResult);
+//print_r(var_dump($leagueRow));
+for($i=0;$i<pg_num_rows($leagueResult);$i++)
+{
+	
+	$leagueRow = pg_fetch_assoc($leagueResult);
+//	echo $leagueRow['id'];
+	//echo "league row ".$leagueRow['leagueID'];
+	//$rosterResult = pg_prepare($connection, "getRosters", "SELECT \"rosterID\", \"username\", \"qb\", \"flex\", \"k\", \"wr\", \"rb\" FROM rosters where \"leagueID\" = '$1'"); 
+	//$rosterResult = pg_execute($connection, "getRosters", array($leagueRow['id']));
+	//print_r(var_dump($rosterResult));
+	$rosterResult = pg_query($connection, "SELECT \"rosterID\", \"username\", \"qb\", \"flex\", \"k\", \"wr\", \"rb\" FROM rosters where \"leagueID\" = '".$leagueRow['id']."'"); 
+	//print_r(var_dump($rosterResult));
+	for($j=0;$j<pg_num_rows($rosterResult);$j++)
+	{
+		
+		$rosterRow = pg_fetch_assoc($rosterResult);	
+		//print_r(var_dump($rosterRow));
+			//echo $rosterRow[2+$k]."AHHA";
+		/*$pointResult = pg_prepare($connection, "getQB", 'SELECT "FantasyPoints" FROM weekplayerdata WHERE id = $1');
+		$pointResult = pg_execute($connection, "getQB", array($rosterRow['qb']));
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];*/
+		//echo $rosterRow['qb']."<br>";
+		$pointResult = pg_query($connection, "SELECT \"FantasyPoints\" FROM weekplayerdata WHERE \"NFLPlayerID\" = '".$rosterRow['qb']."'");
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+	
+		$pointResult = pg_query($connection, "SELECT \"FantasyPoints\" FROM weekplayerdata WHERE \"NFLPlayerID\" = '".$rosterRow['flex']."'");
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+		
+		$pointResult = pg_query($connection, "SELECT \"FantasyPoints\" FROM weekplayerdata WHERE \"NFLPlayerID\" = '".$rosterRow['wr']."'");
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+		
+		$pointResult = pg_query($connection, "SELECT \"FantasyPoints\" FROM weekplayerdata WHERE \"NFLPlayerID\" = '".$rosterRow['k']."'");
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+		
+		$pointResult = pg_query($connection, "SELECT \"FantasyPoints\" FROM weekplayerdata WHERE \"NFLPlayerID\" = '".$rosterRow['rb']."'");
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+		
+	//	print_r(var_dump($pointRow));
+	//	echo $pointRow['FantasyPoints'];
+		/*$pointResult = pg_prepare($connection, "getFlex", 'SELECT "FantasyPoints" FROM weekplayerdata WHERE id = $1');
+		$pointResult = pg_execute($connection, "getFlex", array($rosterRow['flex']));
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+		
+		$pointResult = pg_prepare($connection, "getKick", 'SELECT "FantasyPoints" FROM weekplayerdata WHERE id = $1');
+		$pointResult = pg_execute($connection, "getKick", array($rosterRow['k']));
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+			
+
+		$pointResult = pg_prepare($connection, "getWR", 'SELECT "FantasyPoints" FROM weekplayerdata WHERE id = $1');
+		$pointResult = pg_execute($connection, "getWR", array($rosterRow['wr']));
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+	
+		$pointResult = pg_prepare($connection, "getRB", 'SELECT "FantasyPoints" FROM weekplayerdata WHERE id = $1');
+		$pointResult = pg_execute($connection, "getRB", array($rosterRow['rb']));
+		$pointRow = pg_fetch_assoc($pointResult);
+		$totalPoints = $totalPoints + $pointRow['FantasyPoints'];
+
+		//echo $rosterRow['username'].": $totalPoints <br>";
+		//$userToPoints = array($rosterRow['username'] => $totalPoints);
+		$userToPoints[$rosterRow['username']] = $totalPoints;
+		$totalPoints = 0;*/
+		
+		$userToPoints[$rosterRow['username']] = $totalPoints;
+		$totalPoints = 0;
+
+	}
+	asort($userToPoints);	
+	//print_r(var_dump($userToPoints));
+	//sort($userToPoints);
+	//echo var_dump($userToPoints)."<br>";
+	
+	$tieArray = $userToPoints;
+	//print_r(var_dump($tieArray));
+	$tieArray = array_unique($tieArray, SORT_NUMERIC);
+	//print_r(var_dump($tieArray));
+	sort($tieArray);
+	
+	//print_r(var_dump($tieArray));
+	//echo $tieArray[0];	
+	for($z=0;$z<sizeof($tieArray);$z++)
+	{
+		foreach($userToPoints as $use => $fantPoints)
+		{
+		//	echo $tieArray[$z] ." " . $fantPoints;
+			//echo "$use has: $fantPoints and ".$tieArray[$use]."<br>";
+			if($tieArray[$z]==$fantPoints)
+			{
+				//echo $use." gets ".($z+1)." place<br>";
+				//unset($userToPoints[$use]);
+				if(($z+1) == 1)
+				{
+					//echo "$use won<br>";
+					$updateWinsresult = pg_query($connection, "UPDATE users set \"won\" = \"won\"+1 where \"username\" = '".$use."'");
+				}
+				else
+				{
+					//echo "$use lost<br>";
+					$updateLossresult = pg_query($connection, "UPDATE users set \"lost\" = \"lost\"+1 where \"username\" = '".$use."'");
+	
+				}
+				//echo "$use got ".($z+1)."<br>";
+				$updatePreviousPlace = pg_query($connection, "UPDATE users set \"previousGamePlace\" = ".($z+1)." where \"username\" = '".$use."'");
+					
+			}
+			else
+			{
+				//echo $use." gets ". ($z+1)." place<br>";
+			}
+		}
+	}
+
+	//enter in order of array with place	
+	unset($tieArray);
+	unset($userToPoints);
+	$userToPoints = array(); 	
+	$tieArray = array();
+	
+
+	//print_r(var_dump($userToPoints));
+}
+
+
+?>
--- PHP Interraction/UpdatePlayerSlotIDPerWeek/updateWeekScript.php
+++ PHP Interraction/UpdatePlayerSlotIDPerWeek/updateWeekScript.php
@@ -1,5 +1,8 @@
 <?php
 include "dbconnect.php";
+include "reviewLeagues.php";
+
+
 $zero = '0';
 $result = pg_prepare($connection, "updateauction", "update  seasonplayerdata set \"CurrentAuctionValue\"=w.\"FantasyPoints\" from weekplayerdata w where w.\"NFLPlayerID\"=seasonplayerdata.\"NFLPlayerID\" AND w.\"SlotID\" <> '0'");
 //$result = pg_prepare($connection, "updateauction", 'update  seasonplayerdata set "CurrentAuctionValue"=w."FantasyPoints" from weekplayerdata w where w."NFLPlayerID"=seasonplayerdata."NFLPlayerID" AND w."SlotID" <> '.$zero);
@@ -16,19 +19,30 @@ if (!$presult) {
   exit;
 }
 $row = pg_fetch_assoc($presult);
-
-echo $row['weekNum'];
-if($row['weekNum']<10)
-{
-	$row['weekNum'] = '0'.$row['weekNum'];	
+$weekNum = $row['weeknum'];
+//echo var_dump($row);
+$presult = pg_prepare($connection, "updateweek", 'Update weekNumber set weeknum=$1');
+$presult = pg_execute($connection, "updateweek", array($weekNum+1));
+if (!$presult) {
+  echo "An error occurred.\n";
+  exit;
 }
-echo $row['weekNum'];
+if(($weekNum+1)<10)
+{	
+	$row['weeknum'] = $row['weeknum']+1;
+	$row['weeknum'] = '0'.($row['weeknum']);	
+}
+else
+{
+	$row['weeknum'] = $row['weeknum'] + 1;
+}
+//echo $row['weeknum'];
 //echo "hi";
 $curl = curl_init();
 // Set some options - we are passing in a useragent too here
 	curl_setopt_array($curl, array(
     		CURLOPT_RETURNTRANSFER => 1,
-   		 CURLOPT_URL => 'http://babbage.cs.missouri.edu/~cssgf3/capstone/changevalues/updateWeek.php?week='.$row['weekNum'],
+   		 CURLOPT_URL => 'http://babbage.cs.missouri.edu/~cssgf3/capstone/changevalues/updateWeek.php?week='.$row['weeknum'],
    		 CURLOPT_USERAGENT => 'Simulating Player Game'
 		));
 	// Send the request & save response to $resp
@@ -39,9 +53,29 @@ $result = pg_prepare($connection, "clearweekdata", 'update weekplayerdata set "C
 $result = pg_execute($connection, "clearweekdata", array());
 if(!$result)
 {
-	echo "No update<br>";
+	echo "Error clearing weekdata<br>";
+}
+/*
+$result = pg_prepare($connection, "deleterosters", 'DELETE FROM rosters ;');
+$result = pg_execute($connection, "deleterosters", array());
+if (!$result) {
+  echo "An error occurred resetting rosters.\n";
+  exit;
 }
 
+$presult = pg_prepare($connection, "deleteleagues", 'DELETE FROM leagues;');
+$presult = pg_execute($connection, "deleteleagues", array());
+if (!$presult) {
+  echo "An error occurred resetting leagues.\n";
+  exit;
+}
+
+$presult = pg_prepare($connection, "clearusersdata", "UPDATE users set \"leagueName\" = '',\"rosterID\" = 0 ;");
+$presult = pg_execute($connection, "clearusersdata", array());
+if (!$presult) {
+  echo "An error occurred resetting users rosterID and leaguename.\n";
+  exit;
+}*/
 ?>
 
 
